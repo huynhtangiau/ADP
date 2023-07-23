f1b9ed1e5708dbd44860ae1a046a03f29c0f492f
Giau <giau.huynh@saigontechnology.com>
2023-07-23 21:51:08 +0700

    study project

---
+++ src/Acme.BookStore.Domain.Shared/Localization/LocailizationHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Localization;
+
+namespace Acme.BookStore.Localization
+{
+    public static class LocailizationHelper
+    {
+        public static string L(this string name)
+        {
+            return LocalizableString.Create<BookStoreResource>(name).ResourceName;
+        }
+    }
+}
